44ab9e9a39b234cd1a52cb5af35c595dae10491a
Richard Van Nairn <rvannairn@googlemail.com>
2022-03-28 14:54:12 +0100

    Cypress installed and home page tests written

---
+++ cypress/integration/testing-retyedie-home.js
@@ -0,0 +1,25 @@
+/// <reference types="cypress" />
+
+context("Testing Re Tie Dye Home page", () => {
+    beforeEach(() => {
+        cy.visit("/index.html")
+    })
+
+    it("Locating main h3 on Home Page", () => {
+        cy.get("h3").contains("So, how does this work?")
+    })
+
+    it("Locating the 4th li in the ul on the home page", () => {
+        cy.get("ul").find('li').eq(3)
+    })
+
+    it("Locate Get Bidding call to action button", () => {
+        cy.get("[role='button']").should("exist")
+    })
+
+    it("Locate Get Bidding call to action button make sure it takes you to the correct page once clicked", () => {
+        cy.contains("Get Bidding!").click()
+        cy.url().should('include', 'http://localhost:55958/shop.html')
+        
+    })
+})
